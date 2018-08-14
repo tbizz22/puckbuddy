c5103161db0b8ef4d5df9f2a795b5cdc65eb6a01
Tyler Bisbee <Tyler.Bisbee@gmail.com>
2018-08-14 14:21:50 -0400

    working auth

--- routes/dbUpdate-routes.js
+++ routes/dbUpdate-routes.js
@@ -4,27 +4,23 @@ var btoa = require('btoa');
 // This calls my external API
 var request = require('request');
 
-
 //Get keys for sports feed 
 require('dotenv').config()
 const pbConfig = require('../config/puckbuddy.js');
-// Set Key Values
 
-console.log(pbConfig.credentials.token)
+
+// Set Key Values
 const token = pbConfig.credentials.token;
 const password = pbConfig.credentials.password;
-const encode = btoa(token + password);
-
-
+const encode = btoa(token +":"+ password);
+console.log(encode);
 // get daily game schedule 
-
 module.exports = function (app) {
     app.get("/api/UpdateSchedule", function (req, res) {
         var date = pbConfig.dates.Date;
         var season = pbConfig.dates.Season;
-        var url = "https://api.mysportsfeeds.com/v1.2/pull/nhl/"
-        var qURL1 = url + season + "/daily_game_schedule.json?fordate=" + date;
-        var qURL = "https://api.mysportsfeeds.com/v2.0/pull/nhl/2018-2019-regular/date/20190316/games.json"
+        var url = "https://api.mysportsfeeds.com/v2.0/pull/nhl/"
+        var qURL = url + season + "/date/" + date + "/games.json";
 
         var options = {
             url: qURL,
@@ -36,11 +32,13 @@ module.exports = function (app) {
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
                 var info = JSON.parse(body);
-                console.log(info.stargazers_count + " Stars");
-                console.log(info.forks_count + " Forks");
+                console.log(info);
+              
+            } else {
+                console.log(error);
+                console.log(response.statusCode);
             }
         };
-
 
         request(options, callback);
     })
