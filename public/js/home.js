d249bef21f3d78924c38c5f02535eface94a7e99
tyler bisbee <tyler.bisbee@gmail.com>
2018-08-16 00:10:11 -0400

    working redirect

--- public/js/home.js
+++ public/js/home.js
@@ -14,3 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
   });
+
+
+
+
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,22 @@
 // htmlRoutes.js
 
-
+const btoa = require('btoa');
 const db = require("../models");
 const pbConfig = require('../config/puckbuddy.js');
 const moment = require("moment");
+const request = require("request");
+
+
+// Set Key Values
+const token = pbConfig.credentials.token;
+const password = pbConfig.credentials.password;
+const encode = btoa(token + ":" + password);
+
+
+
+
+
+// Routes
 
 module.exports = function (app) {
     // Load index page
@@ -32,7 +45,49 @@ module.exports = function (app) {
             res.json(err);
         });
     });
+
+
+    app.get("/game/:id", function (req, res) {
+        
+        // improvement is to get team stats from DB here. 
+        
+        
+        
+
+        // get player data direct from API        
+        var id = req.params.id;
+        var season = pbConfig.dates.Season;
+        var url = "https://api.mysportsfeeds.com/v2.0/pull/nhl/"
+        var qURL = url + season + "/games/" + id + "/lineup.json"
+ 
+        var options = {
+            url: qURL,
+            headers: {
+                "Authorization": "Basic " + encode
+            }
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode ==200) {
+                console.log(body);
+
+
+                res.render("game");
+            } else {
+                console.log(error);
+                console.log(statusCode);
+            }
+        }
+ 
+
+        request(options,callback);
+
+    })
+
+
+
 }
+
 
 
 
